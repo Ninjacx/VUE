127fbac5d05da698690bccc01480ecdfeb258914
Ninjacx <291842650@qq.com>
2018-09-30 17:16:23 +0800

    完成购物车界面

--- vue-cli/src/page/car.vue
+++ vue-cli/src/page/car.vue
@@ -24,40 +24,39 @@
               <div class="TextRight marBottom marRight" style="font-size:0;height:.6rem">
                 <span class="Price fs18 marRight">￥138.8</span>
                   <input @click="Add_Sub(1,false,138.8)" type="button" value="-" class="countButton" />
-                  <!-- NowCounts.id==1?NowCounts.Count:16 -->
                   <input maxlength="3"  @keyup="InputCount({id:1},$event)" type="text"  :value="SelCount(1)"  class="countButton2" />
                   <input @click="Add_Sub(1,true,138.8)" type="button" value="+" class="countButton" />
               </div>
             </div>
         </div>
-        <div class="borderBottom padLeft">
-          <div style="display:table-cell;vertical-align:middle;" class=" padRight">
-            <i @click="check({id:2,price:136.6},$event)" :class="['icon iconfont',IsCheckAll?'icon-gouxuan':'icon-gouxuan1']"></i>
-          </div>
-          <div style="display:table-cell;vertical-align:middle;" class=" padRight">
-            <img src="http://img2.imgtn.bdimg.com/it/u=380612834,2294025216&amp;fm=27&amp;gp=0.jpg"  width="100px" class="load-img">
-          </div>
-          <div class="fs17" style="display:table-cell;vertical-align:middle">
-            <div class="">
-               野人谷甩棍
+          <div class="borderBottom padLeft">
+            <div style="display:table-cell;vertical-align:middle;" class=" padRight">
+              <i @click="check({id:2,price:136.6},$event)" :class="['icon iconfont',IsCheckAll?'icon-gouxuan':'icon-gouxuan1']"></i>
             </div>
-            <div class="">
-              黑色金刚版
+            <div style="display:table-cell;vertical-align:middle;" class=" padRight">
+              <img src="http://img2.imgtn.bdimg.com/it/u=380612834,2294025216&amp;fm=27&amp;gp=0.jpg"  width="100px" class="load-img">
             </div>
-          </div>
-            <div class="TextRight marBottom marRight" style="font-size:0;height:.6rem;">
-              <span class="Price fs18 marRight">￥136.6  </span>
-              <input @click="Add_Sub(2,false,136.6)" type="button" value="-" class="countButton" />
-              <input   @keyup="InputCount({id:2},$event)" type="text"  :value="NowCounts.goodsID==2?NowCounts.Count:SelCount(2)"  class="countButton2" />
-              <input  @click="Add_Sub(2,true,136.6)" type="button" value="+" class="countButton" />
+            <div class="fs17" style="display:table-cell;vertical-align:middle">
+              <div class="">
+                 野人谷甩棍
+              </div>
+              <div class="">
+                黑色金刚版
+              </div>
             </div>
-      </div>
+              <div class="TextRight marBottom marRight" style="font-size:0;height:.6rem;">
+                <span class="Price fs18 marRight">￥136.6  </span>
+                <input @click="Add_Sub(2,false,138.8)" type="button" value="-" class="countButton" />
+                <input maxlength="3"  @keyup="InputCount({id:2},$event)" type="text"  :value="SelCount(2)"  class="countButton2" />
+                <input @click="Add_Sub(2,true,138.8)" type="button" value="+" class="countButton" />
+              </div>
+        </div>
       </div>
         <div class="Computed_Car bgWhite fs17">
             <div style="padding: 0 .2rem">
               <span>全选</span>
               <i @click="checkAll()" :class="['icon iconfont',IsCheckAll?'icon-gouxuan':'icon-gouxuan1']"></i>
-               <span style="float:right;"><span class="marRight">合计：￥<span>{{Pay}}</span></span>
+               <span style="float:right;"><span class="marRight">合计：<span class="Price">￥ {{Pay}}</span></span>
                   <mt-button size="small" type="primary">结算</mt-button>
                </span>
              </div>
@@ -108,12 +107,10 @@ export default {
         // console.log(goodInfo);
         this.PayMent += resPrice;
         event.currentTarget.className = gouxuan
-
         this.SelectShop_Arr.push(goodInfo);
       }else{
         event.currentTarget.className = gouxuan1
         this.PayMent -= this.SelCount(obj.id)*obj.price;
-        // this.SelectShop_Arr.$remove();
         let resArr = this.SelectShop_Arr.filter((item,index)=>{
           return item.id != arg.id
         })
@@ -125,40 +122,32 @@ export default {
    checkAll(){
      this.IsCheckAll = !this.IsCheckAll;
      if(this.IsCheckAll){
-      //  this.PayMent = 0;//ajax 所有此用户的订单，取出后算计总和
-         //计算总价格
-        // console.log();
-        let Pay = 0
-        for(let item of this.SleCount){
-            Pay += item.price*item.count
-        }
          this.SelectShop_Arr = this.SleCount
-         this.PayMent = Pay;
+         this.total();
      }else{
        this.SelectShop_Arr = [];//清空选择的商品
        this.PayMent = 0;
      }
    }, InputCount(vid,event){
+     let str = event.currentTarget.value;
+      let IntType = /[^0-9]*$/
+      let rep = str.replace(IntType, "");
+      event.currentTarget.value = rep
      // console.log(this.$data.NowCount);
-     for(let item of this.SleCount){
+      for(let item of this.SleCount){
           if(item.id==vid.id){
             item.count = event.currentTarget.value
           }
       }
-       // this.SelectShop_Arr = this.SleCount
-      // console.log(JSON.stringify(this.SleCount));
-      // this.SelCount(vid.id); //更新新数量
-
-
       if(this.isSelGoods(vid.id)){
-        //循环所有
-        let Pay = 0
         for(let item of this.SelectShop_Arr){
-            Pay += item.price*item.count
+            if(item.id == vid.id){
+               item.count = event.currentTarget.value
+            }
         }
-         this.PayMent = Pay;
+        //循环所有
+        this.total();
       }
-
    } ,
    //根据传入goodsid 获取数量
    SetGoodsID_GetCount(goodsID,is_Add = 0){
@@ -166,7 +155,8 @@ export default {
       // alert(isAdd);
       for(let item of this.SleCount){
           if(item.id==goodsID){
-            item.count = (parseFloat(item.count)+isAdd)
+            item.count = (parseInt(item.count)+isAdd)<0?0:(parseInt(item.count)+isAdd)
+            console.log(item.count);
             return item.count;
           }
       }
@@ -174,8 +164,8 @@ export default {
    Add_Sub(id,isAdd,price){
      //增加 goodsID:0,Count:0
      this.NowCount = {goodsID:id,Count:this.SetGoodsID_GetCount(id,isAdd,price)};
-     let is_SelGoods = this.isSelGoods(id);
 
+     let is_SelGoods = this.isSelGoods(id);
      for(let item of this.SelectShop_Arr){
          if(item.id==id){
            item.count = this.NowCount.Count
@@ -184,19 +174,9 @@ export default {
      console.log(JSON.stringify(this.SelectShop_Arr));
      // 计算总计
      if(is_SelGoods){
-       let Pay = 0
-       for(let item of this.SelectShop_Arr){
-           Pay += item.price*item.count
-       }
-        this.PayMent = Pay;
+       this.total();
      }
      // console.log(this.isSelGoods(id));
-     // this.PayMent += price;
-     // console.log(this.SelCount(id));
-     // console.log(this.NowCounts.goodsID);
-     // console.log(this.NowCounts.Count);
-     // console.log(this.SetGoodsID_GetCount(id,isAdd));
-     // console.log(JSON.stringify(this.SleCount));
    },
    //查询实际选择的数量
    SelCount(goodsID){
@@ -212,6 +192,13 @@ export default {
              return 1;
            }
       }
+   },//计算所有选中的总金额
+   total(){
+         let Pay = 0
+         for(let item of this.SelectShop_Arr){
+             Pay += item.price*item.count
+         }
+         this.PayMent = parseFloat(Pay).toFixed(2);
    }
  }
 }
