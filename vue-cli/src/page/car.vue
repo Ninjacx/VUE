2f889091f848d4826e0a5f7026284142bca65e5e
Ninjacx <291842650@qq.com>
2018-09-30 15:28:40 +0800

    购物车

--- vue-cli/src/page/car.vue
+++ vue-cli/src/page/car.vue
@@ -25,7 +25,7 @@
                 <span class="Price fs18 marRight">￥138.8</span>
                   <input @click="Add_Sub(1,false,138.8)" type="button" value="-" class="countButton" />
                   <!-- NowCounts.id==1?NowCounts.Count:16 -->
-                  <input  @keyup="InputCount({id:1},$event)" type="text"  :value="NowCounts.goodsID==1?NowCounts.Count:SelCount(1)"  class="countButton2" />
+                  <input maxlength="3"  @keyup="InputCount({id:1},$event)" type="text"  :value="SelCount(1)"  class="countButton2" />
                   <input @click="Add_Sub(1,true,138.8)" type="button" value="+" class="countButton" />
               </div>
             </div>
@@ -48,7 +48,7 @@
             <div class="TextRight marBottom marRight" style="font-size:0;height:.6rem;">
               <span class="Price fs18 marRight">￥136.6  </span>
               <input @click="Add_Sub(2,false,136.6)" type="button" value="-" class="countButton" />
-              <input  @keyup="InputCount({id:2},$event)" type="text"  :value="NowCounts.goodsID==2?NowCounts.Count:SelCount(2)"  class="countButton2" />
+              <input   @keyup="InputCount({id:2},$event)" type="text"  :value="NowCounts.goodsID==2?NowCounts.Count:SelCount(2)"  class="countButton2" />
               <input  @click="Add_Sub(2,true,136.6)" type="button" value="+" class="countButton" />
             </div>
       </div>
@@ -73,8 +73,8 @@ export default {
   data () {
     return {
       SleCount:[], //商品的数量集合
-      SelectShop_Arr:[], //所有选中的商品
-      AllShop:[],  //未用到
+      SelectShop_Arr:[], //所有选中的商品数据0
+      // AllShop:[],  //未用到
       IsCheckAll:false, //是否全部选中
       count:0,//总件数
       PayMent:0, //付款金额
@@ -83,7 +83,7 @@ export default {
   },
   mounted: function () {
    this.$store.commit('Flagborder', '3')
-   this.SleCount = [{id:1,price:138.8,count:16},{id:2,price:136.6,count:10}];//初始化把产品对应数量保存进去
+   this.SleCount = [{id:1,price:138.8,count:1},{id:2,price:136.6,count:1}];//初始化把产品对应数量保存进去
  },
  computed: {
    //合计金额
@@ -104,9 +104,12 @@ export default {
       if(ThisClass == gouxuan1){
         // obj.price
         let resPrice = this.SelCount(obj.id)*obj.price
+        let goodInfo = {id:obj.id,price:obj.price,count:this.SelCount(obj.id)}
+        // console.log(goodInfo);
         this.PayMent += resPrice;
         event.currentTarget.className = gouxuan
-        this.SelectShop_Arr.push(obj);
+
+        this.SelectShop_Arr.push(goodInfo);
       }else{
         event.currentTarget.className = gouxuan1
         this.PayMent -= this.SelCount(obj.id)*obj.price;
@@ -124,17 +127,38 @@ export default {
      if(this.IsCheckAll){
       //  this.PayMent = 0;//ajax 所有此用户的订单，取出后算计总和
          //计算总价格
+        // console.log();
+        let Pay = 0
+        for(let item of this.SleCount){
+            Pay += item.price*item.count
+        }
+         this.SelectShop_Arr = this.SleCount
+         this.PayMent = Pay;
      }else{
+       this.SelectShop_Arr = [];//清空选择的商品
        this.PayMent = 0;
      }
    }, InputCount(vid,event){
-     console.log(this.$data.NowCount);
+     // console.log(this.$data.NowCount);
      for(let item of this.SleCount){
           if(item.id==vid.id){
             item.count = event.currentTarget.value
           }
       }
-      console.log(JSON.stringify(this.SleCount));
+       // this.SelectShop_Arr = this.SleCount
+      // console.log(JSON.stringify(this.SleCount));
+      // this.SelCount(vid.id); //更新新数量
+
+
+      if(this.isSelGoods(vid.id)){
+        //循环所有
+        let Pay = 0
+        for(let item of this.SelectShop_Arr){
+            Pay += item.price*item.count
+        }
+         this.PayMent = Pay;
+      }
+
    } ,
    //根据传入goodsid 获取数量
    SetGoodsID_GetCount(goodsID,is_Add = 0){
@@ -152,9 +176,19 @@ export default {
      this.NowCount = {goodsID:id,Count:this.SetGoodsID_GetCount(id,isAdd,price)};
      let is_SelGoods = this.isSelGoods(id);
 
+     for(let item of this.SelectShop_Arr){
+         if(item.id==id){
+           item.count = this.NowCount.Count
+         }
+     }
+     console.log(JSON.stringify(this.SelectShop_Arr));
      // 计算总计
      if(is_SelGoods){
-
+       let Pay = 0
+       for(let item of this.SelectShop_Arr){
+           Pay += item.price*item.count
+       }
+        this.PayMent = Pay;
      }
      // console.log(this.isSelGoods(id));
      // this.PayMent += price;
